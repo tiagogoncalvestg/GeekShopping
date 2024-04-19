806c41fded1f8c3ce4e239eb102c8748ad1395fe
Tiago Gonçalves <tiagogoncalves.tg@hotmail.com>
2024-04-19 08:35:33 -0300

    IProductRepository implementado.

---
+++ Store.ProductApi/Infrastructure/Contracts/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Store.ProductApi.Models.Dtos;
+
+namespace Store.ProductApi.Infrastructure.Contracts;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<ProductDto>> GetAll();
+    Task<ProductDto> FindById(Guid id);
+    // TODO: Alterar para retornar statuscode
+    Task<ProductDto> Create(ProductDto productDto);
+    Task<ProductDto> Update(ProductDto productDto);
+    Task<bool> Delete(Guid id);
+}
---
+++ Store.ProductApi/Models/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.ProductApi.Models.Dtos;
+
+public class ProductDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public string? ImageUrl { get; set; }
+}
